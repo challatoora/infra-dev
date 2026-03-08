fa6b1343252ae88f67cbb213bda0aeb4f1073974
challatoor murali <challatoor murali.gmail.com>
2026-03-08 19:15:29 +0530

    tf

--- 40databases/bootstrap.sh
+++ 40databases/bootstrap.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+component=$1
+environment=$2
 dnf install ansible -y
+
+cd /home/ec2-user
+git clone https://github.com/challatoora/ansible-roboshop-tf.git
+
+cd ansible-roboshop-tf
+
+ansible-playbook -e component=$component roboshop.yaml
