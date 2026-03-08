e06e4f2e0d2e459d366219c5481039e3495a199d
challatoor murali <challatoor murali.gmail.com>
2026-03-08 20:56:45 +0530

    tf

--- 40databases/bootstrap.sh
+++ 40databases/bootstrap.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+set -ex
 
 component=$1
-#environment=$2
-dnf install ansible -y
+
+# Install dependencies
+sudo dnf install -y python3 python3-pip git python3-libdnf
+pip3 install ansible
 
 cd /home/ec2-user
 
+# Clone repo
+rm -rf ansible-roboshop-tf
 git clone https://github.com/challatoora/ansible-roboshop-tf.git
-
 cd ansible-roboshop-tf
 
-ansible-playbook -e component=$component -e ansible_python_interpreter=/usr/bin/python3 roboshop.yaml
+# Create inventory
+echo "[${component}]" > inventory
+echo "localhost ansible_connection=local" >> inventory
+
+# Run playbook with Python 3
+ansible-playbook -i inventory -e component=$component -e ansible_python_interpreter=/usr/bin/python3 roboshop.yaml
