d124ada1a5263b361cc52029df310735a836689d
challatoor murali <challatoor murali.gmail.com>
2026-03-08 20:31:04 +0530

    tf

--- 40databases/bootstrap.sh
+++ 40databases/bootstrap.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-set -ex
 
 component=$1
-
-# Install required packages
-sudo dnf install -y python3 python3-pip git
-
-# Install latest ansible using pip (more stable)
-pip3 install ansible
+#environment=$2
+dnf install ansible -y
 
 cd /home/ec2-user
 
-rm -rf ansible-roboshop-tf
 git clone https://github.com/challatoora/ansible-roboshop-tf.git
 
 cd ansible-roboshop-tf
