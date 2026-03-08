b7ca17dcbad915602eded9fff87fdd5c4d06594a
challatoor murali <challatoor murali.gmail.com>
2026-03-08 22:00:47 +0530

    tf

--- 40databases/variables.tf
+++ 40databases/variables.tf
@@ -5,3 +5,11 @@ variable "project" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id"{
+    default = "Z03767891OMNNRGY4CE9B"
+}
+
+variable "domain_name"{
+    default = "mreddy.online"
+}
