a42351c07cb44d1943fb35f047476d765f46a16e
challatoor murali <challatoor murali.gmail.com>
2026-03-16 14:47:40 +0530

    tf

--- 60catalogue/local.tf
+++ 60catalogue/local.tf
@@ -1,16 +1,11 @@
 locals {
-  backend= data.aws_ssm_parameter.catalogue_sg_id.value
-}
+  catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+  ami_id          = data.aws_ami.joindevops.id
+  subnet          = split(",", data.aws_ssm_parameter.private_subnet_id.value)[0]
 
-locals {
-    subnet=split(",",data.aws_ssm_parameter.private_subnet_id.value)[0]
-
-
-
-    common_tags = {
-        Project = var.project
-        Environment = var.environment
-        Terraform = "true"
+  common_tags = {
+    Project     = var.project
+    Environment = var.environment
+    Terraform   = "true"
   }
-
 }
