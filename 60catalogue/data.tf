265b5427a91bf9abf13849bdd937f31df98836cd
challatoor murali <challatoor murali.gmail.com>
2026-03-19 15:01:24 +0530

    tf

--- 60catalogue/data.tf
+++ 60catalogue/data.tf
@@ -33,3 +33,6 @@ data "aws_ssm_parameter" "private_subnet_id" {
 data "aws_ssm_parameter" "backend_alb_sg_id" {
     name = "/${var.project}/${var.environment}/backend_alb_sg_id"
 }
+data "aws_ssm_parameter" "backend_alb_listener_arn" {
+    name = "/${var.project}/${var.environment}/backend_alb_listener_arn"
+}
