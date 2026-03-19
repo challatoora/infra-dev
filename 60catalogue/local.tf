09675f5005ce8b1609577dffdb6154069ec84a61
challatoor murali <challatoor murali.gmail.com>
2026-03-19 14:35:18 +0530

    tf

--- 60catalogue/local.tf
+++ 60catalogue/local.tf
@@ -2,7 +2,7 @@ locals {
   vpc_id          =data.aws_ssm_parameter.vpc_id.value
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   ami_id          = data.aws_ami.joindevops.id
-  backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
+  backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_sg_id.value
   subnet          = split(",", data.aws_ssm_parameter.private_subnet_id.value)[0]
 
   common_tags = {
