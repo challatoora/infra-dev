3106f53e8ee71ae3120049a89c60448aa45d361d
challatoor murali <challatoor murali.gmail.com>
2026-03-20 14:45:56 +0530

    tf

---
+++ 70caw/variables.tf
@@ -0,0 +1,17 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id"{
+    default = "Z03767891OMNNRGY4CE9B"
+}
+
+variable "domain_name"{
+    default = "mreddy.online"
+}
+
+
