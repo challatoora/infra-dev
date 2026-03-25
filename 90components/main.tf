83258eff0d416af7594ff729dfd50eca8beb7777
challatoor murali <challatoor murali.gmail.com>
2026-03-25 20:39:02 +0530

    tf

---
+++ 90components/main.tf
@@ -0,0 +1,6 @@
+module "component" {
+    for_each = var.components
+    source = "git::https://github.com/challatoora/terraform-roboshop-component.git"
+    component = each.key
+    rule_priority = each.value.rule_priority
+}
