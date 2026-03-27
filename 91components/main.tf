eec5320c8c448936746fa86e78bac3c58e159f5a
challatoor murali <challatoor murali.gmail.com>
2026-03-27 16:52:16 +0530

    tf

--- 91components/main.tf
+++ 91components/main.tf
@@ -1,5 +1,5 @@
 module "component" {
     for_each = var.components
-    source = "git::https://github.com/challatoora/terraform-component.git"
+    source = "git::https://github.com/challatoora/terraform-component.git?ref=main"
     rule_priority = each.value.rule_priority
 }
