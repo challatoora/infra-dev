1c94aef472c8844b2e9c4ff2afe74bfe828c15c7
challatoor murali <challatoor murali.gmail.com>
2026-03-27 14:50:23 +0530

    tf

--- 90 components/bootstrap.sh
+++ 90 components/bootstrap.sh
@@ -10,4 +10,4 @@ git clone https://github.com/challatoora/ansible-roboshop-tf.git
 
 cd ansible-roboshop-roles-tf
 git pull
-ansible-playbook -e component=$component  roboshop.yaml
+ansible-playbook -e component=$component -e env=$environment roboshop.yaml
