8587ce87d5282edba1a733f8eae9ac5e7f046054
challatoor murali <challatoor murali.gmail.com>
2026-03-30 15:11:53 +0530

    tf

--- 20rules/main.tf
+++ 20rules/main.tf
@@ -292,23 +292,25 @@ resource "aws_security_group_rule" "frontend_alb_public" {
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = local.frontend_alb_sg_id
 }
-resource "aws_security_group_rule" "frontend_frontend_alb" { 
-  type = "ingress"
-  from_port = 80
-  to_port = 80
-  protocol = "tcp"
-  source_security_group_id = local.frontend_alb_sg_id
-  security_group_id = local.frontend_id
-}
 
-resource "aws_security_group_rule" "frontend_alb_public" { 
-  type = "ingress"
-  from_port = 443
-  to_port = 443
-  protocol = "tcp"
-  cidr_blocks =["0.0.0.0/0"]
-  security_group_id = local.frontend_alb_sg_id
-}
+#
+# resource "aws_security_group_rule" "frontend_frontend_alb" { 
+#   type = "ingress"
+#   from_port = 80
+#   to_port = 80
+#   protocol = "tcp"
+#   source_security_group_id = local.frontend_alb_sg_id
+#   security_group_id = local.frontend_id
+# }
+
+# resource "aws_security_group_rule" "frontend_alb_public" { 
+#   type = "ingress"
+#   from_port = 443
+#   to_port = 443
+#   protocol = "tcp"
+#   cidr_blocks =["0.0.0.0/0"]
+#   security_group_id = local.frontend_alb_sg_id
+# }
 
 
 
